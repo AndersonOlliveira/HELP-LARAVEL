4aed3e1ffe27139a53c6e09e13701457067e7f3b
AndersonOlliveira <anderson.deolliveira@hotmail.com>
2026-03-29 18:15:12 -0300

    criando projeto com tailwind para o sistema de chamados

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+<body>
+    <nav class="bg-gray-800 p-4">...</nav>
+    <main>
+        <!-- #region -->
+        @yield('content')
+    </main>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/*  ROTA PADADRAO DO LARAVEL**/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+// ROTA PARA TELA DE LOGIN DO PROJETO
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.logon')->with('login', 'Faça login para acessar o sistema');
+    // return view('auth.logon')->name('login');
+})->name('login');
